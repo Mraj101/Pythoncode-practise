357996146459b0f74fa718ecdb19caba39459ab2
Hasnain <mrajhasnain101@gmail.com>
2022-09-16 23:09:19 +0600

    models

--- DjangoPractise/mysite/pollApp/models.py
+++ DjangoPractise/mysite/pollApp/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    questions=models.CharField(max_length=100)
+    pub_date=models.DateTimeField('date published')
+
+class Choice(models.Model):
+    questions=models.ForeignKey(questions,on_delete=CASCADE)
+    
