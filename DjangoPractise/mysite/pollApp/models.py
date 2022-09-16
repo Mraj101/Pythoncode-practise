f6f6441ffbb45ab1f199e0a9e8a6fa4035c1a9ec
Hasnain <mrajhasnain101@gmail.com>
2022-09-16 23:24:59 +0600

    no migrations to apply

--- DjangoPractise/mysite/pollApp/models.py
+++ DjangoPractise/mysite/pollApp/models.py
@@ -1,12 +1,12 @@
-from pyexpat import model
-from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+
 class Question(models.Model):
-    questions=models.CharField(max_length=100)
-    pub_date=models.DateTimeField('date published')
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
 
 class Choice(models.Model):
-    questions=models.ForeignKey(questions,on_delete=CASCADE)
-    
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
