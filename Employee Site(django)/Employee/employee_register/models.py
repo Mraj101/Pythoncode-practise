9b399483c86e1074b86e676c73c28fd7aecc8196
Hasnain <mrajhasnain101@gmail.com>
2022-11-10 23:55:41 +0600

    models created and migrated

--- Employee Site(django)/Employee/employee_register/models.py
+++ Employee Site(django)/Employee/employee_register/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    title=models.CharField(max_length=20)
+
+class Eomployee(models.Model):
+    fullName=models.CharField(max_length=100)
+    phoneNumber=models.CharField(max_length=20)
+    empCode=models.CharField(max_length=3)
+    position=models.ForeignKey(Position,on_delete=models.CASCADE)
